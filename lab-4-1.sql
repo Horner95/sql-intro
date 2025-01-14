4da6168d78d791a747b1536a43ed5eb9bfd6a1a7
Will Horner <will.horner@kellogg.northwestern.edu>
2025-01-14 22:30:17 +0000

    completed lab 4 parts 1-3

--- lab-4-1.sql
+++ lab-4-1.sql
@@ -4,3 +4,8 @@
 -- 2935
 
 
+SELECT players.first_name, players.last_name, SUM(stats.hits)
+FROM stats
+INNER JOIN players ON stats.player_id = players.id
+WHERE players.first_name = "Barry" and players.last_name = "Bonds"
+GROUP BY players.first_name, players.last_name
