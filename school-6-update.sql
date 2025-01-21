d51cdc6ec394f4600dab32db1176308573949889
Will Horner <will.horner@kellogg.northwestern.edu>
2025-01-21 20:13:14 +0000

    modified db for school examples

---
+++ school-6-update.sql
@@ -0,0 +1,5 @@
+-- UPDATE students SET email = "block@hey.com"
+
+-- updates value for email column for ALL ENTRIES
+
+UPDATE students SET email = "block@heygmail.com" WHERE id = 1
---
+++ school-7-delete.sql
@@ -0,0 +1,4 @@
+-- DELETE FROM students;
+-- Deletes all entries, but leaves table formatting
+
+DELETE FROM students WHERE id = 1;
